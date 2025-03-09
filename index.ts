5db8c7f8d54537b312e6bab98657369e09b9c4be
Toni D <tonidy@outlook.com>
2025-03-10 05:44:09 +0700

    Add PID and URL

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
 import { createSqsServer } from "local-aws-sqs";
 
-const server = await createSqsServer({ port: 5450 });
+const port = 5450;
+const server = await createSqsServer({ port });
+
+console.log(`SQS Server running at http://localhost:${port} with PID: ${process.pid}`);
